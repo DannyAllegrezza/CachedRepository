8658f3597b2eb7db056806e6d64b22e7c71ce09b
Danny Allegrezza <danny@dannyallegrezza.com>
2018-09-27 15:58:16 -0400

    Return the results of the database, without any caching layer

--- src/CachedRepository/Pages/Index.cshtml.cs
+++ src/CachedRepository/Pages/Index.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using CachedRepository.Data.Models;
 using CachedRepository.Data.Repositories;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CachedRepository.Pages
 {
 	public class IndexModel : PageModel
     {
         private readonly IRepository<VehicleManufacturer> _repository;
+		public List<VehicleManufacturer> Manufacturers { get; set; }
 
         public IndexModel(IRepository<VehicleManufacturer> repository)
         {
@@ -15,7 +18,7 @@ namespace CachedRepository.Pages
 
         public void OnGet()
         {
-
+			Manufacturers = _repository.GetAll().ToList();
         }
     }
 }
